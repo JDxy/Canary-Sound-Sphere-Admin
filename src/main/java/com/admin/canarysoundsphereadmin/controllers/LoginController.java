15daf1aa7cead8d500d7c9b033c954ede0ca9161
AIPONCEP <ana_ponce_perez@hotmail.es>
2024-04-28 19:46:57 +0200

    login y table-view vistas finalizadas
    falta ajustar si se puede modo dia y noche

--- src/main/java/com/admin/canarysoundsphereadmin/controllers/LoginController.java
+++ src/main/java/com/admin/canarysoundsphereadmin/controllers/LoginController.java
@@ -3,10 +3,9 @@ package com.admin.canarysoundsphereadmin.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import org.json.JSONException;
-
 import java.io.IOException;
-
 import static com.admin.canarysoundsphereadmin.controllers.MethodsForControllers.cambiarScene;
 import static com.admin.canarysoundsphereadmin.models.DBManager.findAdmin;
 
@@ -15,13 +14,16 @@ public class LoginController {
     public Label title;
     @FXML
     public TextField nameTextField;
-
     @FXML
     public TextField passwordTextField;
 
     public void sendButton() throws IOException, JSONException {
-       if (findAdmin(nameTextField.getText(), passwordTextField.getText())){
-           cambiarScene("/com/admin/canarysoundsphereadmin/tables-view.fxml", "Tablas", title);
+        if (findAdmin(nameTextField.getText(), passwordTextField.getText())){
+            cambiarScene("/com/admin/canarysoundsphereadmin/tables-view.fxml", "Tablas", title);
         };
+    }
+
+    public void toggleModeButton(MouseEvent mouseEvent) {
+        System.out.println("clic");
     }
 }
